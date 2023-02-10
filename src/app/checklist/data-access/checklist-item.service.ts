5af2a68cd0c855af88117a8c47a03b9cff53798c
Tay CJ <cjtay@outlook.sg>
2023-02-10 20:54:15 +0800

    checklist interface

---
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {
+  AddChecklistItem,
+  ChecklistItem,
+} from '../../shared/interfaces/checklist-item';
+@Injectable({
+  providedIn: 'root',
+})
+export class ChecklistItemService {
+  private checklistItems$ = new BehaviorSubject<ChecklistItem[]>([]);
+
+  getItemsByChecklistId(checklistId: string) {
+    return this.checklistItems$.pipe(
+      map((items) => items.filter((item) => item.checklistId === checklistId))
+    );
+  }
+
+  add(item: AddChecklistItem, checklistId: string) {
+    const newItem = {
+      id: Date.now().toString(),
+      checklistId,
+      checked: false,
+      ...item,
+    };
+    this.checklistItems$.next([...this.checklistItems$.value, newItem]);
+  }
+}
